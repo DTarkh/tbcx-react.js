e87358940dc0e2dd1ef4a1c5a561f89894889eb2
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-08 13:01:24 +0400

    added about page

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Contact from "../ContactPage/Contact";
+import About from "../AboutPage/About";
 
 
 const router = createBrowserRouter([
     { path: '/', element: <Contact/> },
+    { path: '/about', element: <About/> },
 ])
 
 export default router;
