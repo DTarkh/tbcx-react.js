7f9ec340133d3962f6018aad8e9664a118ac81c5
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-09 17:26:52 +0400

    added props to blog items from the parent component

--- src/BlogPage/Blog.jsx
+++ src/BlogPage/Blog.jsx
@@ -1,23 +1,20 @@
 import Navbar from "../Navigation/Navbar";
 import { Main } from "../Main/Main";
 import BlogItem from "./BlogItem";
-import './Blog.css'
+import "./Blog.css";
 
 const Contact = () => {
-
   const data = [
-    { title: 'dawdwa',
-      description: 'efefe'
-    },
-    {},
-    
-  ]
+    { title: "Vocation friends", content: "Vocation friends inspire each other to pursue meaningful paths, supporting personal growth and fostering shared values in their careers.", author: "Liam Carter", image: "https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill" },
+    { title: "He's all that", content: "He’s All That is a modern teen romantic comedy where a social media influencer attempts to transform an unpopular boy into prom king...", author: "Sophia Ramirez", image: "https://via.assets.so/movie.png?id=2&q=95&w=360&h=360&fit=fill",},
+    { title: "Venom", content: "Venom is a dark, sentient alien symbiote that bonds with a host, granting enhanced abilities, aggression, and a monstrous appearance...", author: "John Doe", image: "https://via.assets.so/movie.png?id=3&q=95&w=360&h=360&fit=fill",}
+  ];
 
   return (
     <div>
       <Navbar />
       <Main>
-        <BlogItem />
+        {data.map(item => <BlogItem title={item.title} content= {item.content} author={item.author} image={item.image}/>)}
       </Main>
     </div>
   );
--- src/BlogPage/BlogItem.jsx
+++ src/BlogPage/BlogItem.jsx
@@ -1,21 +1,21 @@
-import './Blog.css'
+import "./Blog.css";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
   return (
-    <div className='blog-container'>
-        <img className="img" src="https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill" alt="Blog Image" />
-        <div className="body">
-
-        <h1>Blog Title</h1>
-        <p>Blog Content</p>
-        <p>Posted by: John Doe</p>
-        <p>Posted on: January 1, 2022</p>
-        <h2>Comments</h2>
-        <button className='btn'>Details</button>
-
-        </div>
+    <div className="blog-container">
+      <img
+        className="img"
+        src={props.image}
+        alt="Blog Image"
+      />
+      <div className="body">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <p>{props.author}</p>
+        <button className="btn">Details</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogItem
+export default BlogItem;
