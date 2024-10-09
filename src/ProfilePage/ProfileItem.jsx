b0f570fb0ca99fce851c30d4112b77e315ac755f
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-09 19:15:10 +0400

    added profile form

--- src/ProfilePage/ProfileItem.jsx
+++ src/ProfilePage/ProfileItem.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Profile.css'
+import React from "react";
+import "./Profile.css";
 
 const ProfileItem = () => {
   return (
-    <div>ProfileItem</div>
-  )
-}
+    <div className="form-container">
+      <img
+        src="https://images.unsplash.com/photo-1506863530036-1efeddceb993?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Form Header"
+        className="form-image"
+      />
+      <form>
+        <input type="text" placeholder="Jhane" className="input-field" />
+        <input type="text" placeholder="Doe" className="input-field" />
+        <input
+          type="email"
+          placeholder="jhanedoe@tbcx.com"
+          className="input-field"
+        />
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default ProfileItem
+export default ProfileItem;
